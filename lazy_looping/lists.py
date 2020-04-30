d099f978ffefeb491fe3ad0b597624294c47007b
srikanth marella <marella.srikanth4@live.com>
2020-04-29 22:45:03 -0600

    added lazy looping samples

---
+++ lazy_looping/lists.py
@@ -0,0 +1,29 @@
+"""List comprehension exercises"""
+
+
+def get_vowel_names():
+    """Return a list containing all names given that start with a vowel."""
+
+
+def flatten():
+    """Return a flattened version of the given 2-D matrix (list-of-lists)."""
+
+
+def matrix_from_string():
+    """Convert rows of numbers to list of lists."""
+
+
+def power_list():
+    """Return a list that contains each number raised to the i-th power."""
+
+
+def matrix_add():
+    """Add corresponding numbers in given 2-D matrices."""
+
+
+def identity():
+    """Return an identity matrix of size x size."""
+
+
+def triples():
+    """Return list of Pythagorean triples less than input num."""
